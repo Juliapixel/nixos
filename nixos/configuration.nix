acb120020fcf9a1ddeb47a10a85c1fab8855a3b2
Juliapixel <git@juliapixel.com>
2025-11-27 05:57:37 -0300

    change to grub with os-prober

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,7 +14,12 @@
   };
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    useOSProber = true;
+    efiSupport = true;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
