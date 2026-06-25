edea2e0f5bb2beefb3a6fbb2374fe04de2f20ddd
Juliapixel <git@juliapixel.com>
2026-06-25 00:47:29 -0300

    update to latest nvidia 580 package

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -53,7 +53,15 @@
     videoAcceleration = true;
     open = false;
     modesetting.enable = true;
-    branch = "legacy_580";
+    # branch = "legacy_580";
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.159.04";
+      sha256_64bit = "sha256-weZnYbCI0Xs632y2l53przi+JoTRArABoXbc+vq9yh4=";
+      sha256_aarch64 = "sha256-iRLyYjvHyDl2Xzb87j20o1MYNKLK/zql1JwSWbI3Kus=";
+      openSha256 = "sha256-zsNmjZW0cyZWPp3vDT3mNeqAo0hS0M7e9Tbvwvij+F4=";
+      settingsSha256 = "sha256-U0hics4gQeZWsD+ch9PBz42zfTOEVcKRVIqYZb3VOY8=";
+      persistencedSha256 = "sha256-vDawiy52GB8JABUKZDiQUc8uda8p/7jCFW7rTu6QMa4=";
+    };
   };
 
   # only add cudaSupport for a few packages cuz i dont wanna rebuild all of nixpkgs
