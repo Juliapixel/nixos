937443d7825c070abc4dada5294131b972a38979
Juliapixel <git@juliapixel.com>
2026-07-05 23:43:09 -0300

    add default noctalia config

--- home-manager/noctalia.nix
+++ home-manager/noctalia.nix
@@ -6,6 +6,73 @@
 {
   programs.noctalia = {
     enable = true;
-    settings = {};
+    settings = {
+      dock = {
+        active_monitor_only = true;
+        auto_hide = true;
+        enabled = true;
+        icon_size = 36;
+        reserve_space = false;
+      };
+
+      notification.position = "bottom_right";
+
+      theme = {
+        source = "wallpaper";
+        wallpaper_scheme = "m3-rainbow";
+      };
+
+      shell = {
+        transparency_mode = "soft";
+      };
+
+      wallpaper = {
+        transition = [ "fade" ];
+        automation = {
+          enabled = true;
+          interval_seconds = 900;
+        };
+      };
+
+      widget = {
+        media = {
+          hide_when_no_media = true;
+          title_scroll = "on_hover";
+        };
+
+        spacer_2.type = "spacer";
+
+        network.show_label = false;
+
+        workspaces.display = "none";
+      };
+
+      lockscreen.tint = 0.5;
+
+      bar.default = {
+        background_opacity = 0.84999998100101948;
+        center = [ "workspaces" ];
+        end = [
+          "tray"
+          "notifications"
+          "clipboard"
+          "network"
+          "bluetooth"
+          "volume"
+          "brightness"
+          "battery"
+          "session"
+        ];
+        margin_ends = 12;
+        start = [
+          "launcher"
+          "clock"
+          "spacer_2"
+          "media"
+        ];
+        thickness = 32;
+        widget_spacing = 12;
+      };
+    };
   };
 }
